b928be5b30d0eb03e16c4fb154497cfe55f5b270
Shuichiro Aiba <shuichiro.aiba@gmail.com>
2014-10-15 23:45:16 +0900

    Add user, group and schedule in models

--- norisuke/models.py
+++ norisuke/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __unicode__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=64)
+    access_id = models.CharField(max_length=64)
+    group = models.ForeignKey(Group)
+
+    def __unicode__(self):
+        return self.name
+
+class Schedule(models.Model):
+    name = models.CharField(max_length=128)
+    desc = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    group = models.ForeignKey(Group)
+
+    def __unicode__(self):
+        return self.name
+
