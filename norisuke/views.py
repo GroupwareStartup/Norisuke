c1a9f9038f55236077732ed28f8ef434358f0fae
Shuichiro Aiba <shuichiro.aiba@gmail.com>
2014-10-17 00:32:30 +0900

    Add top page html

--- norisuke/views.py
+++ norisuke/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from norisuke.models import *
+
+class ScheduleList(request):
+
+    return render(request, "ScheduleList.html", None)
+
--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from norisuke import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.ScheduleList, name='schedule_list'),
 )
