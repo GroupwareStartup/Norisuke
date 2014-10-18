2235f33213610cf6d16d4caf99b2f12e86dd3de1
Shuichiro Aiba <shuichiro.aiba@gmail.com>
2014-10-18 12:30:27 +0900

    Fix for local test environment of DB

--- project/settings.py
+++ project/settings.py
@@ -56,6 +56,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
     'default': {
@@ -63,6 +64,11 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Parse database configuration from $DATABASE_URL
+config = dj_database_url.config()
+if config:
+    DATABASES['default'] = config
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -76,10 +82,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
